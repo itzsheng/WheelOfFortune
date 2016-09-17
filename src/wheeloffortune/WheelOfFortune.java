a2042648822a2e21c1d8efe128074e368b6d608b
Sheng <Sheng@DESKTOP-ETIUA4C>
2016-09-17 01:25:16 -0400

    Sprint 1

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -8,6 +8,8 @@
 */
 package wheeloffortune;
 
+import java.util.Scanner;
+
 public class WheelOfFortune {
 
   /**
@@ -15,6 +17,70 @@ public class WheelOfFortune {
    */
   public static void main(String[] args) {
     
+      Scanner user = new Scanner(System.in);
+
+//		As a user, I want to be able to guess a letter, so I can discover the puzzle.	
+		
+//		char letter;		
+//		System.out.print("Please enter a letter!\n");
+//		letter = user.next().charAt(0);
+//		
+//		if (Character.toString(letter).matches("^[a-zA-Z]+")){
+//			System.out.println("You have selected : " + letter);
+//		}
+//		else {
+//			System.out.println("You have entered more than one letter or a number!");
+//		}
+		
+		
+		
+		
+		
+		
+		System.out.println("Welcome to the Wheel of Fortune\n" + 
+	               "Please select an option:    \n" +
+	               "    1) Spin the Wheel!\n" + 
+	               "    2) Buy a Vowel!\n" + 
+	               "    3) Solve the Puzzle!\n" + 
+	               "    4) Testing\n" +
+	               "    5) Quit\n" +
+	               "    Please enter a number : "
+	       );
+		
+		String input = user.nextLine();
+		int choice = Integer.parseInt(input);
+		
+		switch (choice) {
+		case 1:
+			System.out.println("You chose to spin the wheel!");
+			break;
+		case 2:
+			System.out.println("You chose to buy a vowel!");
+			break;
+		case 3:
+			System.out.println("You chose to solve the puzzle!");
+			break;
+		case 4:
+			//As a QA tester, I want a "test" choice beneath the current user choices on the main menu,
+			//so I can test additional functionality
+			System.out.println("You chose testing\n" + 
+								"Please enter a letter!");
+			char letter;
+			letter = user.next().charAt(0);
+			
+			if (Character.toString(letter).matches("^[a-zA-Z]+")){
+				System.out.println("You have selected : " + letter);
+			}
+			else {
+				System.out.println("You have entered more than one letter or a number!");
+			}
+			break;
+		case 5:
+			System.exit(0);
+			break;
+		default:
+			System.out.println("You didnt chose any of the menu selection!");			
+		}
   }
   
 }
